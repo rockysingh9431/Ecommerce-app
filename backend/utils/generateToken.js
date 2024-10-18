baf5b18b90223047acc9659142bcd8f524d6c5d0
Rocky Singh <100080538+rockysingh9431@users.noreply.github.com>
2024-10-18 10:57:31 +0530

    bug fix

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = (req, res, userId) => {
+const generateToken = (res, userId) => {
   const token = jwt.sign({ userId: userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
